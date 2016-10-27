fbddea65b2e512a2411e55de316cd488d70eda9f
Hakan <hakanmhmd@yahoo.com>
2016-10-27 16:38:36 +0100

    more code refactoring

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ var controller = Botkit.slackbot({
   debug: false
 })
 
-// Assume single team mode if we have a SLACK_TOKEN
 if (slack_token) {
-  console.log('Starting in single-team mode')
   controller.spawn({
     token: slack_token,
     retry: Infinity
@@ -32,39 +30,13 @@ if (slack_token) {
 
     console.log('Connected to Slack RTM')
   })
-// Otherwise assume multi-team mode - setup beep boop resourcer connection
 } 
 
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
-
-/**controller.hears(['hello', 'hi'], ['direct_mention', 'direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+  bot.reply(message, "I'm in!")
 })
 
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
-
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi/hello` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  
-})
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-})
-**/
 
 controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
   var wit = witbot.process(message.text, bot, message)
